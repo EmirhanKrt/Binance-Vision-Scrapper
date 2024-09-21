275b3f4df9468c0c037710dee46df5ac0e1b5e7d
Emirhan Kurt <emirhankurtofc@gmail.com>
2024-09-21 19:13:45 +0300

    Developed file path generator functions

---
+++ src/actions/download-historical-data.ts
@@ -0,0 +1,23 @@
+import { DownloadHistoricalDataFormData } from "@/lib/types";
+
+import { generateDateList, generateDateObject } from "@/lib/utils/date";
+import {
+  generateFileBasePathObject,
+  generateSourceFilePath
+} from "@/lib/utils/source-file-path";
+
+export async function downloadHistoricalData(
+  formData: DownloadHistoricalDataFormData
+) {
+  const startDate = generateDateObject(new Date(formData.StartDate));
+  const endDate = generateDateObject(new Date(formData.EndDate));
+
+  const dateList = generateDateList(formData.DataInterval, startDate, endDate);
+
+  const { sourcePath, destinationPath, fileNamePrefix } =
+    generateFileBasePathObject(formData);
+
+  const sourceFilePathList = dateList.map((date) =>
+    generateSourceFilePath(sourcePath, fileNamePrefix, date)
+  );
+}
